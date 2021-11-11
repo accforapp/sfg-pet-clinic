f37b630ce9aaff31fb9bc0d69b67ed903db83fe0
sergeyshevchenko <serhii.shevchenko.privatbank.ua>
2021-11-11 21:48:01 +0200

    version 1.0.34
    description:
    
    add project lombok.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Speciality.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.models;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "specialties")
 public class Speciality extends BaseEntity {
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/models/Vet.java
@@ -1,9 +1,16 @@
 package guru.springframework.sfgpetclinic.models;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -20,36 +27,4 @@ public class Vet extends Person {
   @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "specialty_id"))
   private Set<Speciality> specialities = new HashSet<>();
-
-  public Set<Speciality> getSpecialities() {
-    return specialities;
-  }
-
-  public void setSpecialities(Set<Speciality> specialities) {
-    this.specialities = specialities;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getCity() {
-    return city;
-  }
-
-  public void setCity(String city) {
-    this.city = city;
-  }
-
-  public String getTelephone() {
-    return telephone;
-  }
-
-  public void setTelephone(String telephone) {
-    this.telephone = telephone;
-  }
 }
